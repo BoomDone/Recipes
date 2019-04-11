3aa88413892906f3c033d03104d157a77c695d86
Kristen Muehlenthaler <ksnare007@gmail.com>
2019-04-11 17:35:18 -0500

    chore(): Jest configurations

---
+++ src/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+        transformIgnorePatterns: [
+    
+            "<rootDir>/node_modules/(?!@ionic-native|@ionic|ionicons)",
+    ],
+    coverageReporters: ['text', 'html'],
+    coverageDirectory: '<rootDir>'
+}
